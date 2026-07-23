1e5e73d2fe5aee6040ad39c348e19ccaeb2cb84e
Igor Ozik Vasconcelos <igorozikvasconcelosfilho@gmail.com>
2026-07-23 19:38:37 -0300

    Update Circulo.java

--- src/figuras/Circulo.java
+++ src/figuras/Circulo.java
@@ -1,1 +1,29 @@
+package figuras;
 
+public class Circulo implements FiguraGeometrica2D {
+
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public double getRaio() {
+        return raio;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * Math.pow(raio, 2);
+    }
+
+    @Override
+    public double getPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Circulo";
+    }
+}
