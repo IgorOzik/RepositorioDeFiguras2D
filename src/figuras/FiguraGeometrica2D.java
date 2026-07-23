d63d0c3be069828a5b40f6eebd8c5dfe94adec4a
Igor Ozik Vasconcelos <igorozikvasconcelosfilho@gmail.com>
2026-07-23 19:49:32 -0300

    Extend FiguraGeometrica2D to implement Comparable

--- src/figuras/FiguraGeometrica2D.java
+++ src/figuras/FiguraGeometrica2D.java
@@ -1,6 +1,6 @@
 package figuras;
 
-public interface FiguraGeometrica2D {
+public interface FiguraGeometrica2D extends Comparable<FiguraGeometrica2D> {
     double getArea();
     double getPerimetro();
     String getTipo();
